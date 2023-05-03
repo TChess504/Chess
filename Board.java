afac556e866597ecb3349afa97d32203c1891c72
TChess504 <taderogba5@gmail.com>
2023-05-03 01:02:21 -0400

    unfinished

---
+++ Board.java
@@ -0,0 +1,16 @@
+
+public class Board {
+	
+	Spot spotArray[][] = new Spot[8][8];
+	
+	public Board() {
+	
+		// Creates board with spots
+		for(int i = 0; i < spotArray.length; i++) {
+			for(int j = 0; j < spotArray.length; j++) {
+				
+			}
+		}
+		
+	}	
+}
---
+++ Spot.java
@@ -0,0 +1,13 @@
+
+public class Spot {
+	
+	private int xPostion;
+	private int yPosition;
+	
+	
+	
+	public Spot() {
+		
+		
+	}
+}
